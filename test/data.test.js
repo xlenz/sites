c753325a1b60acd83bd77f6a28c169e7a48eb06b
Mykola Grybyk <mykola.grybyk@gmail.com>
2022-03-29 23:59:16 +0200

    dns test list

--- test/data.test.js
+++ test/data.test.js
@@ -1,8 +1,12 @@
 test('sites', () => {
   const sites = require('../data/sites.json')
   const matcher = { method: 'get', page: expect.stringMatching(/^https?:\/\//) }
+
   sites
-    .filter((site) => typeof site.ip === 'string')
+    .filter((site) => site.method === 'get' && typeof site.ip === 'string')
     .forEach((site) => expect(site).toEqual({ ...matcher, ip: expect.stringMatching(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/) }))
-  sites.filter((site) => typeof site.ip === 'undefined').forEach((site) => expect(site).toEqual(matcher))
+
+  sites.filter((site) => site.method === 'get' && typeof site.ip === 'undefined').forEach((site) => expect(site).toEqual(matcher))
+
+  sites.filter((site) => site.method === 'dns').forEach((site) => expect(site).toEqual({ method: 'dns', host: expect.any(String), port: expect.any(Number) }))
 })
